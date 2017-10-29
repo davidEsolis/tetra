7f9116e70568dcf11a1400abdb66736cfe95b6ee
Unknown <alphahs2@outlook.com>
2017-10-29 13:17:23 -0700

    wew
    ewe

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
     <!--meta:vp--><meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body style="background: #979797" >
+    <script src="js/push.min.js"></script>
     <?php
 
     
@@ -48,12 +49,83 @@
     <!-- -->
     
        <!--AQUI SE TERMINA EL MENÚ -->
-      <div class="container">
-          <h1>mi texto</h1>
-          <p>
-          
-Closed := Closed ∪ {Node}
-          </p>
-      </div>
+      <section>
+      III. Beneficiados:
+
+
+El número de personas beneficiados en el periodo comprendido del 07 al 28  de Febrero     del 2017, Fueron los alumnos de la facultad de Derecho y Ciencia Política,- Los Mochis, por la	labor de guía durante los tramites asignados referente a cartas de asignación, informes mensuales, carta final, entre otros.
+
+IV. Metodología aplicada:
+
+
+Para aplicar el proyecto se requirieron de aplicar ciertos métodos, para la realización del Servicio Social fueron los siguientes:
+Métodos:
+Método analítico. Método sistemático. Método inductivo. Método de estudio.
+
+V. Técnicas aplicadas:
+
+
+Es el conjunto de reglas y operaciones para el manejo de los  instrumentos  que  auxilian al individuo en la aplicación de los métodos.
+
+*Uso de sistema de Sostware para revision de Informes y cartas de asiganación,	entre otro requerimiento.
+
+VI. Resultados obtenidos:
+
+
+Los resultados obtenidos durante la estancia en la facultad de Derecho y Ciencia Política,- Los Mochis. área de servicio Social, para la realización del proyecto de servicio social, correspondientes al periodo del 07 al 28 de Febrero de 2017.
+
+Contribuir en auxiliar al inicio de mi proyecto, en apoyar a el alumnado en la facultad de Derecho y Ciencia Política. que realizan sus actividades de Brigadistas en la unidad receptora, asi como al Coordinador o responsable de la misma.III. Beneficiados:
+
+
+El número de personas beneficiados en el periodo comprendido del 07 al 28  de Febrero     del 2017, Fueron los alumnos de la facultad de Derecho y Ciencia Política,- Los Mochis, por la	labor de guía durante los tramites asignados referente a cartas de asignación, informes mensuales, carta final, entre otros.
+
+IV. Metodología aplicada:
+
+
+Para aplicar el proyecto se requirieron de aplicar ciertos métodos, para la realización del Servicio Social fueron los siguientes:
+Métodos:
+Método analítico. Método sistemático. Método inductivo. Método de estudio.
+
+V. Técnicas aplicadas:
+
+
+Es el conjunto de reglas y operaciones para el manejo de los  instrumentos  que  auxilian al individuo en la aplicación de los métodos.
+
+*Uso de sistema de Sostware para revision de Informes y cartas de asiganación,	entre otro requerimiento.
+
+VI. Resultados obtenidos:
+
+
+Los resultados obtenidos durante la estancia en la facultad de Derecho y Ciencia Política,- Los Mochis. área de servicio Social, para la realización del proyecto de servicio social, correspondientes al periodo del 07 al 28 de Febrero de 2017.
+
+Contribuir en auxiliar al inicio de mi proyecto, en apoyar a el alumnado en la facultad de Derecho y Ciencia Política. que realizan sus actividades de Brigadistas en la unidad receptora, asi como al Coordinador o responsable de la misma.III. Beneficiados:
+
+
+El número de personas beneficiados en el periodo comprendido del 07 al 28  de Febrero     del 2017, Fueron los alumnos de la facultad de Derecho y Ciencia Política,- Los Mochis, por la	labor de guía durante los tramites asignados referente a cartas de asignación, informes mensuales, carta final, entre otros.
+
+IV. Metodología aplicada:
+
+
+Para aplicar el proyecto se requirieron de aplicar ciertos métodos, para la realización del Servicio Social fueron los siguientes:
+Métodos:
+Método analítico. Método sistemático. Método inductivo. Método de estudio.
+
+V. Técnicas aplicadas:
+
+
+Es el conjunto de reglas y operaciones para el manejo de los  instrumentos  que  auxilian al individuo en la aplicación de los métodos.
+
+*Uso de sistema de Sostware para revision de Informes y cartas de asiganación,	entre otro requerimiento.
+
+VI. Resultados obtenidos:
+
+
+Los resultados obtenidos durante la estancia en la facultad de Derecho y Ciencia Política,- Los Mochis. área de servicio Social, para la realización del proyecto de servicio social, correspondientes al periodo del 07 al 28 de Febrero de 2017.
+
+Contribuir en auxiliar al inicio de mi proyecto, en apoyar a el alumnado en la facultad de Derecho y Ciencia Política. que realizan sus actividades de Brigadistas en la unidad receptora, asi como al Coordinador o responsable de la misma.
+<script>
+    Push.create("hola we");
+</script>
+      </section>
 </body>
 </html>
