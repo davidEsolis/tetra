20c38380d95f2ca40751296bde401ea87638df46
Unknown <alphahs2@outlook.com>
2017-10-30 10:53:39 -0700

    we
    we

--- index.php
+++ index.php
@@ -32,18 +32,17 @@
         <a href="" class="bt-menu"><span class="icon-list2"></span>Menu</a>
     </div>
 <nav>
-        <ul>
+    
             <li><a href="index.php"><span class="icon-home-1" ></span> Inicio |</a></li>
             <li><a href="#" ><span class="icon-atom" ></span> <?php echo " ".$usu ?></a>
                 <ul>
                     <li><a href="vistas/verPerfil.php"><span class="icon-coffee-streamline" ></span> Perfil</a></li>
                     <li><a href=""><span class="icon-chart-line" ></span> Materias |</a></li> 
                     <li><a href=""><span class="icon-settings" ></span> Configuraciones</a>
-                    <ul>
-                    <li><a class="icon-moon"
-                      href="salir.php"> Salir</a></li>
-                    </ul>
-                </li>  
+                     <ul>
+                       <li><a class="icon-moon"href="salir.php"> Salir</a></li>
+                      </ul>
+                    </li>  
                 </ul>
             </li>  
  </nav>
@@ -74,7 +73,7 @@ Es el conjunto de reglas y operaciones para el manejo de los  instrumentos  que 
 *Uso de sistema de Sostware para revision de Informes y cartas de asiganación,	entre otro requerimiento.
 
 VI. Resultados obtenidos:
-
+encia Política,- Los Mochis, por la	labor de guía durante los tramites asignados referente a cartas de asignación, informes mensuales, carta final, entre otros. IV. Metodología aplicada: Para aplicar el proyecto se requirieron de aplicar ciertos métodos, para la realización del Servicio Social fueron los siguientes: Métodos: Método analítico. Método sistemático. Método inductivo. Método de estudio. V. Técnicas aplicadas: Es el conjunto de reglas y operaciones para el manejo de los instrumentos que auxilian al individuo en la aplicación de los métodos. *Uso de sistema de Sostware para revision de Informes y cartas de asiganación,	entre otro requerimiento. VI. Resultados obtenidos: Los resultados obtenidos durante la estancia en la facultad de Derecho y Ciencia Política,- Los Mochis. área de servicio Social, para la realización del proyecto de servicio social, correspondientes al periodo del 07 al 28 de Febrero de 2017. Contribuir en auxiliar al inicio de mi proyecto, en apoyar a el alumnado en la facultad de Derecho y Ciencia Política. que realizan sus actividades de Brigadistas en la unidad receptora, asi como al Coordinador o responsable de la misma.III. Beneficiados: El número de personas beneficiados en el periodo comprendido del 07 al 28 de Febrero del 2017, Fueron los alumnos de la facultad de Derecho y Ciencia Política,- Los Mochis, por la	labor de guía durante los tramites asignados referente a cartas de asignación, informes mensuales, carta final, entre otros. IV. Metodología aplicada: Para aplicar el proyecto se requirieron de aplicar ciertos métodos, para la realización del Servicio Social fueron los siguientes: Métodos: Método analítico. Método sistemático. Método inductivo. Método de estudio. V. Técnicas aplicadas: Es el conjunto de reglas y operaciones para el manejo de los instrumentos que auxilian al individuo en la aplicación de los métodos. *Uso de sistema de Sostware para revision de Informes y cartas de asiganación, entre otro requerimiento. VI. Resultados obtenidos: Los resultados obtenidos durante la estancia en la facultad de Derecho y Ciencia Política,- Los Mochis. área de servicio Social, para la realización del proyecto de servicio social, correspondientes al periodo del 07 al 28 de Febrero de 2017. Contribuir en auxiliar al inicio de mi proyecto, en apoyar a el alumnado en la facultad de Derecho y Ciencia Política. que realizan sus actividades de Brigadistas en la unidad receptora, asi como al Coordinador o responsable de la misma.III. Beneficiados: El número de personas beneficiados en el periodo comprendido del 07 al 28 de Febrero del 2017, Fueron los alumnos de la facultad de Derecho y Ciencia Política,- Los Mochis, por la	labor de guía durante los tramites asignados referente a cartas de asignación, informes mensuales, carta final, entre otros. IV. Metodología aplicada: Para aplicar el proyecto se requirieron de aplicar ciertos métodos, para la realización del
 
 Los resultados obtenidos durante la estancia en la facultad de Derecho y Ciencia Política,- Los Mochis. área de servicio Social, para la realización del proyecto de servicio social, correspondientes al periodo del 07 al 28 de Febrero de 2017.
 
